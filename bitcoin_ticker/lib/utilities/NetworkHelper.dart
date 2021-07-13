d19345f6dc0e1b1841366e7ac134af98d5358ecd
code_with_Syphen <pranavkale021998@gmail.com>
2021-07-13 19:12:34 +0530

    Bitcoin Ticker v2.0.1

--- bitcoin_ticker/lib/utilities/NetworkHelper.dart
+++ bitcoin_ticker/lib/utilities/NetworkHelper.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class NetworkHelper {
-  String _apiKey = "E7538FF4-4458-4C11-9682-25C7585AF09B";    // limit : 100 request/per day.
+  String _apiKey = "";   // get you API key at https://www.coinapi.io/Pricing.
 
   Future<String> getCurrentValue( {required String cryptoType, required String currencyCode})  async {
     String  postUrl = "https://rest.coinapi.io/v1/exchangerate/$cryptoType/$currencyCode/?apikey=";
